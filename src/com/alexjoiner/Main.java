d7f178c18f073ac6b1c57774d5aa1252dee17fce
Alex <ajmarksboy@gmail.com>
2021-04-05 14:44:40 -0500

    Reverse String in Place

--- src/com/alexjoiner/Main.java
+++ src/com/alexjoiner/Main.java
@@ -1,8 +1,12 @@
 package com.alexjoiner;
 
+import com.alexjoiner.problems.ReverseString;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ReverseString reverseString = new ReverseString();
+        char[] string = reverseString.reverseString("regrubmah".toCharArray());
+        System.out.println(string);
     }
 }
