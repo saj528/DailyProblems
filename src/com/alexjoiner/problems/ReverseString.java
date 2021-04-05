94452244d15784fc8c2a88e124fd019363b3c323
Alex <ajmarksboy@gmail.com>
2021-04-05 15:39:20 -0500

    Reverse String Conditional Fix

--- src/com/alexjoiner/problems/ReverseString.java
+++ src/com/alexjoiner/problems/ReverseString.java
@@ -2,7 +2,7 @@ package com.alexjoiner.problems;
 
 public class ReverseString {
     public char[] reverseString(char[] s) {
-        for(int i = 0; i <= s.length / 2; i++){
+        for(int i = 0; i < s.length / 2; i++){
             char tempLast = s[s.length - 1 - i];
             s[s.length - 1 - i] = s[i];
             s[i] = tempLast;
